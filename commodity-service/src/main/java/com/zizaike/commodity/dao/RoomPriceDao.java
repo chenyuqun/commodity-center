571b6ca6d6861a4b4082a0c64ae93affdab34795
snowzhang <snowzhang@zizaike.com>
2016-03-15 20:42:29 +0800

    add

--- commodity-service/src/main/java/com/zizaike/commodity/dao/RoomPriceDao.java
+++ commodity-service/src/main/java/com/zizaike/commodity/dao/RoomPriceDao.java
@@ -5,32 +5,32 @@
  * Date:2016年3月14日下午4:27:06  <br/>
  * Copyright (c) 2016, zizaike.com All Rights Reserved.  
  *  
-*/  
-  
-package com.zizaike.commodity.dao;  
+*/
+
+package com.zizaike.commodity.dao;
 
 import com.zizaike.core.framework.exception.ZZKServiceException;
 import com.zizaike.entity.commodity.RoomPrice;
 
-/**  
- * ClassName:RoomPriceDao <br/>  
- * Function: 房价. <br/>  
- * Date:     2016年3月14日 下午4:27:06 <br/>  
- * @author   snow.zhang  
- * @version    
- * @since    JDK 1.7  
- * @see        
+/**
+ * ClassName:RoomPriceDao <br/>
+ * Function: 房价. <br/>
+ * Date: 2016年3月14日 下午4:27:06 <br/>
+ *
+ * @author snow.zhang
+ * @version
+ * @since JDK 1.7
+ * @see
  */
 public interface RoomPriceDao {
     /**
      * 
-     * add:增加房价. <br/>  
-     *  
-     * @author snow.zhang  
+     * add:增加房价. <br/>
+     * 
+     * @author snow.zhang
      * @param roomPrice
-     * @throws ZZKServiceException  
+     * @throws ZZKServiceException
      * @since JDK 1.7
      */
     void add(RoomPrice roomPrice) throws ZZKServiceException;
 }
-  
--- commodity-service/src/test/java/com/zizaike/commodity/service/RoomPriceTest.java
+++ commodity-service/src/test/java/com/zizaike/commodity/service/RoomPriceTest.java
@@ -5,9 +5,9 @@
  * Date:2016年1月11日下午4:51:02  <br/>
  * Copyright (c) 2016, zizaike.com All Rights Reserved.  
  *  
-*/  
-  
-package com.zizaike.commodity.service;  
+ */
+
+package com.zizaike.commodity.service;
 
 import java.util.Date;
 
@@ -18,37 +18,33 @@ import com.zizaike.commodity.bastest.BaseTest;
 import com.zizaike.core.framework.exception.ZZKServiceException;
 import com.zizaike.entity.commodity.RoomPrice;
 
-/**  
- * ClassName:UserServiceTest <br/>  
- * Function: 房价服务. <br/>  
- * Date:     2016年1月11日 下午4:51:02 <br/>  
- * @author   snow.zhang  
- * @version    
- * @since    JDK 1.7  
- * @see        
+/**
+ * ClassName:UserServiceTest <br/>
+ * Function: 房价服务. <br/>
+ * Date: 2016年1月11日 下午4:51:02 <br/>
+ * 
+ * @author snow.zhang
+ * @version
+ * @since JDK 1.7
+ * @see
  */
-public class RoomPriceTest extends BaseTest{
+public class RoomPriceTest extends BaseTest {
     @Autowired
-    private RoomPriceService roomPriceService; 
+    private RoomPriceService roomPriceService;
+
     @Test(description = "add")
     public void add() throws ZZKServiceException {
-        for (int i = 0; i < 10; i++) {
-            RoomPrice roomPrice = new RoomPrice();
-            roomPrice.setCreateAt(new Date());
-            roomPrice.setRoomTypeId(i);
-            roomPrice.setPrice(123);
-            roomPrice.setCreateUId(123);
-            roomPrice.setUpdateAt(new Date());
-            roomPrice.setUpdateUId(123);
-            roomPrice.setUserId(123);
-            roomPrice.setDate(new Date(1232131223+i));
-            roomPriceService.add(roomPrice);
-        }
-     
+        RoomPrice roomPrice = new RoomPrice();
+        roomPrice.setCreateAt(new Date());
+        roomPrice.setRoomTypeId(2);
+        roomPrice.setPrice(123);
+        roomPrice.setCreateUId(123);
+        roomPrice.setUpdateAt(new Date());
+        roomPrice.setUpdateUId(123);
+        roomPrice.setUserId(123);
+        roomPrice.setDate(new Date());
+        roomPriceService.add(roomPrice);
+
     }
-    
-   
-    
 
 }
-  
